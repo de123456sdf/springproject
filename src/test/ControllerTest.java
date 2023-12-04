67d4d96a1923d7c0249c8208b0d75552dd21dc36
de123456sdf <cdaeun95@gmail.com>
2023-12-04 12:01:45 +0900

    test 추가

---
+++ src/test/ControllerTest.java
@@ -0,0 +1,24 @@
+public class ControllerTest implements CommonTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .build();
+
+        // Mock 테스트 UserDetails 생성
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(TEST_USER);
+
+        // SecurityContext 에 인증된 사용자 설정
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(
+                testUserDetails, testUserDetails.getPassword(), testUserDetails.getAuthorities()));
+    }
+}
